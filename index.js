f7738a9fda4169deb159bfae3edf8dc76e462d6d
衞藤勝也 <masaya@etomasauyanoMacBook-Pro.local>
2021-04-21 00:29:04 +0900

    配列に対する型設定

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ var bool = true;
 var num = 10;
 var str = "hello";
 var person = {
-    name: "佐藤",
-    age: 19
+    name: {
+        first: "田中",
+        last: "太郎"
+    },
+    age: 21
 };
-console.log(person.age);
+console.log(person.name.first);
+var fruites = ["Apple", "Orange", "Banana"];
+console.log(fruites[0]);
+var list = ["Banana", 10, true];
+console.log(list[2]);
--- index.ts
+++ index.ts
@@ -5,12 +5,18 @@ let bool: boolean = true;
 let num: number = 10;
 let str: string = "hello"
 
-let person: {
-  name: string;
-  age: number;
-} = {
-  name: "佐藤",
-  age: 19
+let person = {
+  name: {
+    first: "田中",
+    last: "太郎"
+  },
+  age: 21
 }
 
-console.log(person.age);
+console.log(person.name.first);
+
+const fruites: string[] = ["Apple", "Orange", "Banana"];
+console.log(fruites[0]);
+
+const list: any[] = ["Banana", 10, true];
+console.log(list[2]);
