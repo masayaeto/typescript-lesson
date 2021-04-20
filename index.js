35de3e9ffce62febc4917d100ae34e5c5c1b4b63
衞藤勝也 <masaya@etomasauyanoMacBook-Pro.local>
2021-04-21 00:20:59 +0900

    オブジェクト型で型設定を行う

---
+++ index.js
@@ -0,0 +1,10 @@
+var hello = "hello";
+console.log(hello);
+var bool = true;
+var num = 10;
+var str = "hello";
+var person = {
+    name: "佐藤",
+    age: 19
+};
+console.log(person.age);
---
+++ index.ts
@@ -0,0 +1,16 @@
+let hello: string = "hello";
+console.log(hello);
+
+let bool: boolean = true;
+let num: number = 10;
+let str: string = "hello"
+
+let person: {
+  name: string;
+  age: number;
+} = {
+  name: "佐藤",
+  age: 19
+}
+
+console.log(person.age);
